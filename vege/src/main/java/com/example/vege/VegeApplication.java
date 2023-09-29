fc2d73b48c399bb712f25a5bfc97c41da67537fe
junyoung kim <adad568@naver.com>
2023-09-29 21:03:11 +0900

    Update : 삭제 되어서 다시 복구

---
+++ vege/src/main/java/com/example/vege/VegeApplication.java
@@ -0,0 +1,13 @@
+package com.example.vege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegeApplication.class, args);
+	}
+
+}
